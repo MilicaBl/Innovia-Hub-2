f291b5d236179e9e8fa85f1d5d8845387f46ccd1
MilicaBl <milab.code@gmail.com>
2025-10-29 16:20:34 +0100

    db change

---
+++ Backend/Migrations/20251029151128_UpdateModel.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Backend.Migrations
+{
+    /// <inheritdoc />
+    public partial class UpdateModel : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "EndTime",
+                table: "Bookings");
+
+            migrationBuilder.DropColumn(
+                name: "StartTime",
+                table: "Bookings");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTime>(
+                name: "EndTime",
+                table: "Bookings",
+                type: "datetime(6)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "StartTime",
+                table: "Bookings",
+                type: "datetime(6)",
+                nullable: true);
+        }
+    }
+}
